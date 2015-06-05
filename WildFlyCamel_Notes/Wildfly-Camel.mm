acab88107a2bd813e69d0cdc7eca029bae09014d
Rick <rick@localhost.localdomain>
2015-06-05 15:31:52 -0500

    Notes from WFCamel Javadoc

---
+++ WildFlyCamel_Notes/Wildfly-Camel.mm
@@ -0,0 +1,80 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1433533472577" ID="ID_1936917259" MODIFIED="1433535769761" STYLE="fork" TEXT="WIldfly-Camel">
+<node CREATED="1433533518888" FOLDED="true" ID="ID_1351305534" MODIFIED="1433535781970" POSITION="right" TEXT="org.wildfly.extension.camel">
+<node CREATED="1433533701712" ID="ID_1483700726" MODIFIED="1433533821400" TEXT="CamelConstants">
+<icon BUILTIN="flag-green"/>
+</node>
+<node CREATED="1433533713843" ID="ID_283327954" MODIFIED="1433533824521" TEXT="CamelContextFactory">
+<icon BUILTIN="flag-green"/>
+</node>
+<node CREATED="1433533725942" ID="ID_1721090659" MODIFIED="1433533828663" TEXT="CamelContextRegistry">
+<icon BUILTIN="flag-green"/>
+</node>
+<node CREATED="1433533732837" ID="ID_635758367" MODIFIED="1433533832136" TEXT="CamelLogger">
+<icon BUILTIN="flag-green"/>
+</node>
+<node CREATED="1433533745876" ID="ID_480489993" MODIFIED="1433533835586" TEXT="ContextCreateHandler">
+<icon BUILTIN="flag-green"/>
+</node>
+<node CREATED="1433533764932" ID="ID_1378766200" MODIFIED="1433533838878" TEXT="ContextCreateHandlerRegistry">
+<icon BUILTIN="flag-green"/>
+</node>
+<node CREATED="1433533874898" ID="ID_463927057" MODIFIED="1433533883623" TEXT="SpringCamelContextFactory">
+<icon BUILTIN="flag-orange"/>
+</node>
+<node CREATED="1433533887348" ID="ID_1733021987" MODIFIED="1433533897896" TEXT="WildflyCamelContext">
+<icon BUILTIN="flag-orange"/>
+</node>
+</node>
+<node CREATED="1433533600272" ID="ID_1806100946" MODIFIED="1433535769760" POSITION="right" TEXT="org.wildfly.extension.camel.handler"/>
+<node CREATED="1433533626223" FOLDED="true" ID="ID_232819017" MODIFIED="1433535769760" POSITION="left" TEXT="org.wildfly.extension.camel.parser">
+<node CREATED="1433533933503" ID="ID_349908375" MODIFIED="1433534071924" TEXT="CamelExtension">
+<icon BUILTIN="flag-orange"/>
+<node CREATED="1433534024176" ID="ID_1421069656" MODIFIED="1433534026295" TEXT="Domain extension used to initialize the Camel subsystem."/>
+</node>
+<node CREATED="1433533942650" ID="ID_1080122028" MODIFIED="1433533948420" TEXT="SubsystemState">
+<icon BUILTIN="flag-orange"/>
+<node CREATED="1433534034085" ID="ID_1338981677" MODIFIED="1433534069234" TEXT="The Camel subsystem state."/>
+</node>
+</node>
+<node CREATED="1433533642074" FOLDED="true" ID="ID_693684572" MODIFIED="1433536265218" POSITION="right" TEXT="org.wildfly.extension.camel.security">
+<node CREATED="1433534763150" ID="ID_1468886403" MODIFIED="1433534770691" TEXT="AuthorizationPolicy">
+<icon BUILTIN="flag"/>
+<node CREATED="1433534786111" ID="ID_1934083139" MODIFIED="1433534787234" TEXT="Provides access RunAs login policy"/>
+</node>
+<node CREATED="1433534791206" ID="ID_1817097645" MODIFIED="1433534812156" TEXT="ClientLoginContext">
+<icon BUILTIN="flag"/>
+<node CREATED="1433534814166" ID="ID_251662955" MODIFIED="1433534815534" TEXT="Provides access RunAs Client login context"/>
+</node>
+</node>
+<node CREATED="1433533650481" FOLDED="true" ID="ID_426846879" MODIFIED="1433535775688" POSITION="left" TEXT="org.wildfly.extension.camel.service">
+<node CREATED="1433535160327" FOLDED="true" ID="ID_1804260501" MODIFIED="1433535769761" TEXT="CamelBootstrapService">
+<icon BUILTIN="flag-orange"/>
+<node CREATED="1433535178176" ID="ID_870901140" MODIFIED="1433535180720" TEXT="Service responsible for creating and managing the life-cycle of the Camel subsystem."/>
+</node>
+<node CREATED="1433535204393" FOLDED="true" ID="ID_1961738225" MODIFIED="1433535769761" TEXT="CamelContextFactoryBindingService">
+<icon BUILTIN="flag-orange"/>
+<node CREATED="1433535210846" ID="ID_710082020" MODIFIED="1433535210846" TEXT="">
+<node CREATED="1433535229354" ID="ID_976649268" MODIFIED="1433535229354" TEXT="The CamelContextFactory JNDI binding service"/>
+</node>
+</node>
+<node CREATED="1433535283242" FOLDED="true" ID="ID_673595901" MODIFIED="1433535769761" TEXT="CamelContextFactoryService">
+<icon BUILTIN="flag"/>
+<node CREATED="1433535288066" ID="ID_558258880" MODIFIED="1433535292746" TEXT="The CamelContextFactoryService service."/>
+</node>
+<node CREATED="1433535591751" ID="ID_217270193" MODIFIED="1433535769761" TEXT=" CamelContextRegistryBindingService  ">
+<icon BUILTIN="flag-orange"/>
+<node CREATED="1433535596228" ID="ID_729660440" MODIFIED="1433535769761" TEXT="The  CamelContextRegistry JNDI binding svc."/>
+</node>
+<node CREATED="1433535627511" ID="ID_1547253134" MODIFIED="1433535769761" TEXT="CamelContextRegistryService">
+<icon BUILTIN="flag-orange"/>
+</node>
+<node CREATED="1433535672851" ID="ID_796315177" MODIFIED="1433535769761" TEXT="ContextCreateHandlerRegistryService">
+<icon BUILTIN="flag-orange"/>
+<node CREATED="1433535678096" ID="ID_1214750493" MODIFIED="1433535769761" TEXT="Has addService() and start()"/>
+</node>
+</node>
+<node CREATED="1433533661057" ID="ID_148152939" MODIFIED="1433535769761" POSITION="left" TEXT="org.wildfly.extension.camel.deployment"/>
+</node>
+</map>
