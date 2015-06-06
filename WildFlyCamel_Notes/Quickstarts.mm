4c1d843a05ad1536b21231fa1f1faf160cf0edf7
Rick Wagner <RickJWag@yahoo.com>
2015-06-06 15:42:34 -0500

    Add quickstart notes

---
+++ WildFlyCamel_Notes/Quickstarts.mm
@@ -0,0 +1,20 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1433622152693" ID="ID_688101220" MODIFIED="1433622246067" TEXT="Camel-on-EAP-quickstarts">
+<node CREATED="1433622174731" ID="ID_1634986055" MODIFIED="1433622193480" POSITION="right" TEXT="camel-activemq">
+<node CREATED="1433622387245" ID="ID_608302950" MODIFIED="1433623276342" TEXT="RB: ActiveMQRouteBuilder">
+<node CREATED="1433622538365" ID="ID_496878923" MODIFIED="1433622542124" TEXT="@Startup @ApplicationScoped @ContextName(&quot;amq-cdi-context&quot;)"/>
+<node CREATED="1433622551019" ID="ID_1314929082" MODIFIED="1433622591337" TEXT="makes A-MQ component in &apos;configure()&apos;"/>
+<node CREATED="1433623276342" ID="ID_1680459029" MODIFIED="1433623301270" TEXT="So just uses &apos;startup&apos; and sticks in CDI, the route runs (no other startup references)"/>
+</node>
+</node>
+<node CREATED="1433622248905" ID="ID_249526027" MODIFIED="1433622251804" POSITION="left" TEXT="camel-cdi"/>
+<node CREATED="1433622253825" ID="ID_1856674766" MODIFIED="1433622257700" POSITION="right" TEXT="camel-cxf"/>
+<node CREATED="1433622267809" ID="ID_271699591" MODIFIED="1433622271288" POSITION="left" TEXT="camel-jaxws"/>
+<node CREATED="1433622273299" ID="ID_36900856" MODIFIED="1433622277654" POSITION="right" TEXT="camel-jpa"/>
+<node CREATED="1433622279634" ID="ID_1751184868" MODIFIED="1433622281542" POSITION="left" TEXT="camel-jms"/>
+<node CREATED="1433622294836" ID="ID_1801058753" MODIFIED="1433622298431" POSITION="right" TEXT="camel-mail"/>
+<node CREATED="1433622300230" ID="ID_886907030" MODIFIED="1433622302803" POSITION="left" TEXT="camel-rest"/>
+<node CREATED="1433622304755" ID="ID_175123175" MODIFIED="1433622308493" POSITION="right" TEXT="camel-transacted-jms"/>
+</node>
+</map>
